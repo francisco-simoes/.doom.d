7ef2811c9f4a8ec54b990b9c560e789c7d59827a
fsimoes <francisconfqsimoes@gmail.com>
2022-11-23 18:26:24 +0100

    White smoke background for pdfs when using leuven.

--- config.el
+++ config.el
@@ -516,6 +516,18 @@ checkboxes."
 (load-theme 'leuven-dark t)
 ;; (setq doom-theme 'nil)
 
+;; light gray as pdf background when using Leuven theme:
+(defface my-special-face '((t :background "white smoke"))
+  "My custom background.")
+(add-hook 'pdf-view-mode-hook
+        (lambda ()
+                (if (eq doom-theme 'leuven)
+                    ;; (set-background-color "white smoke")
+                    (face-remap-add-relative 'default 'my-special-face)
+                )
+        )
+)
+
 ;; Change directory to save the "desktops" in
 (setq desktop-dirname "/home/fsimoes/.doom.d/desktop_save")
 ;; Make it save automatically
@@ -830,3 +842,102 @@ checkboxes."
  :desc "Start fuzzy finder in home directory"
  :leader
  :n "o z" #'fsimoes-fzf-home)
+
+;;;;; TO SCROLL OTHER WINDOW COMPATIBLE WITH PDFS: from https://gist.github.com/politza/3f46785742e6e12ba0d1a849f853d0b9#file-scroll-other-window-el
+;;; sow.el --- Variable commands for scrolling the other window.
+
+;; Copyright (C) 2016  Andreas Politz
+
+;; Author: Andreas Politz <politza@fh-trier.de>
+;; Keywords: extensions, frames
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(defvar-local sow-scroll-up-command nil)
+
+(defvar-local sow-scroll-down-command nil)
+
+(defvar sow-mode-map
+  (let ((km (make-sparse-keymap)))
+    (define-key km [remap scroll-other-window] 'sow-scroll-other-window)
+    (define-key km [remap scroll-other-window-down] 'sow-scroll-other-window-down)
+    km)
+  "Keymap used for `sow-mode'")
+
+(define-minor-mode sow-mode
+  "FIXME: Not documented."
+  nil nil nil
+  :global t)
+
+(defun sow-scroll-other-window (&optional arg)
+  (interactive "P")
+  (sow--scroll-other-window-1 arg))
+
+(defun sow-scroll-other-window-down (&optional arg)
+  (interactive "P")
+  (sow--scroll-other-window-1 arg t))
+
+(defun sow--scroll-other-window-1 (n &optional down-p)
+  (let* ((win (other-window-for-scrolling))
+         (cmd (with-current-buffer (window-buffer win)
+		(if down-p
+		    (or sow-scroll-down-command #'scroll-up-command)
+		  (or sow-scroll-up-command #'scroll-down-command)))))
+    (with-current-buffer (window-buffer win)
+      (save-excursion
+        (goto-char (window-point win))
+        (with-selected-window win
+          (funcall cmd n))
+        (set-window-point win (point))))))
+
+(add-hook 'Info-mode-hook
+	  (lambda nil
+	    (setq sow-scroll-up-command
+		  (lambda (_) (Info-scroll-up))
+		  sow-scroll-down-command
+		  (lambda (_) (Info-scroll-down)))))
+
+(add-hook 'doc-view-mode-hook
+          (lambda nil
+            (setq sow-scroll-up-command
+                  'doc-view-scroll-up-or-next-page
+                  sow-scroll-down-command
+                  'doc-view-scroll-down-or-previous-page)))
+
+(add-hook 'pdf-view-mode-hook
+          (lambda nil
+            (setq sow-scroll-up-command
+                  'pdf-view-scroll-down-or-previous-page
+                  ;; 'pdf-view-scroll-up-or-next-page
+                  sow-scroll-down-command
+                  'pdf-view-scroll-up-or-next-page)))
+                  ;; 'pdf-view-scroll-down-or-previous-page)))
+
+(provide 'scroll-other-window)
+;;; scroll-other-window.el ends here
+
+
+;; Keybind the above functions
+(map!
+ :g "C-M-j" #'sow-scroll-other-window-down)
+
+(map!
+ :g "C-M-k" #'sow-scroll-other-window)
